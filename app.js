e0fd21dbdae819d58655a5d83cb21a5143523af1
User Name <ahmedalnahham1187@email.com>
2024-11-12 16:15:59 +0300

    very Hard

--- app.js
+++ app.js
@@ -1,9 +1,65 @@
 /*-------------------------------- Constants --------------------------------*/
-
+const performCalculation = {
+    '+': (firstOperand, secondOperand) => firstOperand + secondOperand,
+    '-': (firstOperand, secondOperand) => firstOperand - secondOperand,
+    '*': (firstOperand, secondOperand) => firstOperand * secondOperand,
+    '/': (firstOperand, secondOperand) => firstOperand / secondOperand
+}
 /*-------------------------------- Variables --------------------------------*/
-
+let displayValue = '0'
+let firstOperand = null
+let secondOperand = false
+let opertor = null
 /*------------------------ Cached Element References ------------------------*/
+const calculator = document.querySelector('#calculator')   
+const display = document.querySelector('#display')
 
 /*----------------------------- Event Listeners -----------------------------*/
-
+calculator.addEventListener('click', handleButtonClock)     //Add event listeners to all buttons at once.
 /*-------------------------------- Functions --------------------------------*/
+
+/////////////////////////////////////////////////////////////////
+function updateDisplay() {
+    display.textContent = displayValue;
+}
+
+function handleButtonClock(event) {
+    const {target} = event
+}
+
+
+if (target. classList.contains('number')) {
+    if (displayValue === '0'|| secondOperand) {
+        displayValue = target.innerText
+        secondOperand = false
+    } else {
+        displayValue += target.innerText
+    }
+    updateDisplay()
+}
+/////////////////////////////////////////////////////////////////
+
+
+if (target.classList.contains('opertor')){
+    if (firstOperand === null) {
+        firstOperand = parseFloat(displayValue)
+    } else if (opertor) {
+        const result = performCalculation[opertor](firstOperand, parseFloat(displayValue))
+        displayValue = `${result}`
+        firstOperand = result
+    }
+    opertor = target.innerText
+secondOperand = true
+updateDisplay()
+}
+/////////////////////////////////////////////////////////////////////
+
+
+
+function resetCalculator () {
+    displayValue = '0'
+    firstOperand = null
+    opertor = null
+    secondOperand = false
+}
+//////////////////////////////////////////////////////////////////////
