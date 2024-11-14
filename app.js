567c9e4e1b79b9a629bd35f84ea857612e00dc84
User Name <ahmedalnahham1187@email.com>
2024-11-14 19:57:37 +0300

    tnanks very much Zainb was vry helpful

--- app.js
+++ app.js
@@ -1,65 +1,66 @@
 /*-------------------------------- Constants --------------------------------*/
 /*-------------------------------- Variables --------------------------------*/
-let firstNumber = '';
-let secondNumber = '';
+let fN = '';
+let sN = '';
 let operator = null;
 let result = 0;
+
 /*------------------------ Cached Element References ------------------------*/
-const buttons = document.querySelectorAll('.button')
+const buttons = document.querySelector('.button')
 const displayEl = document.querySelector('.display')
 /*----------------------------- Event Listeners -----------------------------*/
-buttons.forEach((button) => {
-    button.addEventListener('click', (event) => {
-        // if it is a number
-        if(event.target.classList.contains("number")) {
+buttons.forEach(buttons ) => {
+    buttons.addEventListener('click', (event)  => {
+        //if it is a number
+        if(event.target.classList.contains('number')) {
             if(operator === null) {
-                firstNumber += event.target.innerText
-                displayEl.innerText = firstNumber
+                fN += event.tartget.innerText
+                displayEl.innerText = fN
             } else {
-                secondNumber += event.target.innerText
-                displayEl.innerText = firstNumber + operator + secondNumber 
+                sN += event.targt.innerText
+                displayEl.innerText = fN + operator + sN
+                
             }
         }
-        // if it is an operator
-        else if (event.target.classList.contains("operator")){
-            let selectedOperator = event.target.innerText
-            if (selectedOperator === 'C') {
-              // CLEAR DISPLAY
-              displayEl.innerText = ''
-              firstNumber = ''
-              secondNumber = ''
-              operator = null
-              result = 0
-            } else {
-                if (firstNumber !== '') {
-                    operator = event.target.innerText   
-                    displayEl.innerText = firstNumber + operator
-                }
+        // if it is an operator    
+    else if (event.targt.classList.contains('operator')){
+        let selectedOperator = event.target.innerText
+
+        // CLEAR DISPLAY
+        if (selectedOperator === 'C') {
+            displayEl.innerText = ''
+            fN = ''
+            sN = ''
+            operator = null
+            result = 0
+        } else {
+            if (fN !== '') {
+                operator = event.target.innerText 
+                displayEl.innerText = fN + operator
             }
-            
-        } 
-        // if it's equals
-        
-        else if (event.target.classList.contains("equals")) {
-            calculate()
         }
+    }
+    // if its equals
+    else if (event.targt.classList.contains('equals')){
+        calculate()
+    }
     })
-})
+}
 /*-------------------------------- Functions --------------------------------*/
 function calculate() {
-    if(firstNumber !== '' && secondNumber !== '' && operator !== null) {
-        let num1 = parseFloat(firstNumber)
-        let num2 = parseFloat(secondNumber)
+    if(fN !== '' && sN !== '' && operator !== null ) {
+        let num1 = parseFloat(fN)
+        let num2 = parseFloat(sN)
         // do the calculation
         if (operator === '+') {
-            result = num1 + num2
+            result = num1 + num2 
         } else if(operator === '-') {
             result = num1 - num2
         } else if(operator === '*') {
-            result = num1 * num2
+            result = num1 * num2 
         } else if(operator === '/') {
             result = num1 / num2
         }
-        displayEl.innerText = result
-    }
-}
+    } 
+}  displayEl.innerText = result
+       
